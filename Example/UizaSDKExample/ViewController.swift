e8c0ab8b941fb92400fa89108c04bf319dfceebf
Nam Kennic <namkennic@me.com>
2019-02-19 16:47:32 +0700

    Updated example to show how to use delegate callback

--- Example/UizaSDKExample/ViewController.swift
+++ Example/UizaSDKExample/ViewController.swift
@@ -109,6 +109,8 @@ class ViewController: UIViewController {
 			if let videos = results, let video = videos.randomElement() {
 				DispatchQueue.main.async {
 					let viewController = FloatingPlayerViewController()
+					viewController.delegate = self
+					viewController.player.delegate = self
 					viewController.present(with: video).player.controlView.theme = UZTheme1()
 					viewController.floatingHandler?.allowsCornerDocking = true
 				}
@@ -125,6 +127,42 @@ extension ViewController: UITextFieldDelegate {
 		_ = textField.resignFirstResponder()
 		loadVideo()
 		return true
+	}
+	
+}
+
+extension ViewController: UZPlayerDelegate {
+	
+	func UZPlayer(player: UZPlayer, playerStateDidChange state: UZPlayerState) {
+		// called when player state was changed (buffering, buffered, readyToPlay ...)
+	}
+	
+	func UZPlayer(player: UZPlayer, loadedTimeDidChange loadedDuration: TimeInterval, totalDuration: TimeInterval) {
+		// called when loaded duration was changed
+	}
+	
+	func UZPlayer(player: UZPlayer, playTimeDidChange currentTime: TimeInterval, totalTime: TimeInterval) {
+		// called when player time was changed
+	}
+	
+	func UZPlayer(player: UZPlayer, playerIsPlaying playing: Bool) {
+		// called when playing state was changed
+	}
+	
+}
+
+extension ViewController: UZFloatingPlayerViewProtocol {
+	
+	func floatingPlayer(_ player: UZFloatingPlayerViewController, didBecomeFloating: Bool) {
+		// called when floating player became floating mode or backed to normal mode
+	}
+	
+	func floatingPlayer(_ player: UZFloatingPlayerViewController, onFloatingProgress: CGFloat) {
+		// called when user drag the player from normal mode to floating mode
+	}
+	
+	func floatingPlayerDidDismiss(_ player: UZFloatingPlayerViewController) {
+		// called when player was dismissed
 	}
 	
 }
